d1a1673471e625674e3f1ab02307383005f5c900
Surajgandal <89153620+Surajgandal@users.noreply.github.com>
2022-02-10 01:03:24 +0530

    Add files via upload

---
+++ home/js/slider.js
@@ -0,0 +1,46 @@
+ var swiper1 = new Swiper(".swiper-container1", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true
+        },
+		loop:true,
+		autoplay:{
+			delay:2000,
+			disableOnInteraction:false,
+		
+			
+		},
+        pagination: {
+          el: ".swiper-pagination1",
+        }
+      });
+	  
+	  
+	  
+	  var swiper2 = new Swiper('.swiper-container2', {
+        effect: "cube",
+        grabCursor: true,
+        cubeEffect: {
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        },
+		loop:true,
+		autoplay:{
+			delay:1000,
+			disableOnInteraction:false,
+		
+			
+		},
+        pagination: {
+          el: ".swiper-pagination2",
+        },
+      });
